f4c407211c66bb8ee4a22eb72e8490a3ed1402cc
MakosLab <mmakos@ua.edu>
2025-01-29 10:43:35 -0600

    Add pytorch test

--- pyTorch_CPU/pytorch_test.py
+++ pyTorch_CPU/pytorch_test.py
@@ -1,16 +1,17 @@
 import torch
 
-# Check if PyTorch is using CPU
+# Check if pyTorch is using CPU 
+# It should print False because CUDA works on GPUs only
 print(f"Is CUDA available? {torch.cuda.is_available()}")
 print(f"Device being used: {torch.device('cpu')}")
 
-# Create a simple tensor
+# Create your first tensor
 x = torch.tensor([1.0, 2.0, 3.0])
 print(f"Tensor: {x}")
 
-# Perform a basic operation
+# Let it do the math 
 y = x * 2
 print(f"Tensor after multiplication: {y}")
 
-# Print PyTorch version
+# Print pyTorch version beacuse why not 
 print(f"PyTorch Version: {torch.__version__}")
